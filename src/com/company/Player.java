1b3004d06914678d4c28367307c5f8b587f0ac16
otter-lights <rachelroutly@cmail.carleton.ca>
2020-11-23 14:40:13 -0500

    Added the constructors for the playerRace attribute to the Player constructor

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,6 +1,5 @@
 package com.company;
 import java.util.Random;
-import java.util.stream.Stream;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -12,7 +11,7 @@ public class Player {
     Race playerRace;
     int strength;
     int dexterity;
-    int constiution;
+    int constitution;
     int intelligence;
     int wisdom;
     int charisma;
@@ -20,9 +19,41 @@ public class Player {
     int[] money = new int[3]; // depends on class
     ArrayList<Item> inventory;
 
-    public Player(){
-
+    //The chosen race input will come from the gui/a generation
+    //constructor is currently empty (maybe not the best way to implement?)
+    public Player(String chosenRace){
+        if(chosenRace.equals("Gnome")){
+            playerRace = new Gnome();
+        }
+        else if(chosenRace.equals("Halfling")){
+            playerRace = new Halfling();
+        }
+        else if(chosenRace.equals("Dwarf")){
+            playerRace = new Dwarf();
+        }
+        else if(chosenRace.equals("Dragonborn")){
+            playerRace = new Dragonborn();
+        }
+        else if(chosenRace.equals("Elf")){
+            playerRace = new Elf();
+        }
+        else if(chosenRace.equals("HalfElf")){
+            playerRace = new HalfElf();
+        }
+        else if(chosenRace.equals("HalfOrc")){
+            playerRace = new HalfOrc();
+        }
+        else if(chosenRace.equals("Human")){
+            playerRace = new Human();
+        }
+        else if(chosenRace.equals("Tiefling")){
+            playerRace = new Tiefling();
+        }
+        else{
+            System.out.println("Invaild Race");
+        }
     }
+
     // A players carrying capacity depends on the size of their Race, this function allows the item to be added to inventory as long as the total weight of the items is under their carrying capacity
     // For now I am ignoring the decrease in speed that comes with higher weights, potentially added at a later date.
     public void addToInventory(Item newItem){
@@ -69,7 +100,7 @@ public class Player {
         //ignoring class primary stat
         strength = abilityScores[0];
         dexterity = abilityScores[1];
-        constiution = abilityScores[2];
+        constitution = abilityScores[2];
         intelligence = abilityScores[3];
         wisdom = abilityScores[4];
         charisma = abilityScores[5];
