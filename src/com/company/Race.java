0520731a0786b08093b31147dfff347eca0c45b3
otter-lights <rachelroutly@cmail.carleton.ca>
2020-11-22 19:06:46 -0500

    Added some stuff to Race class

--- src/com/company/Race.java
+++ src/com/company/Race.java
@@ -1,4 +1,8 @@
 package com.company;
 
 public class Race {
+    String size;
+    int speed;
+    //this will be a 6 int array containing the base ability score increases for each race.
+    int[] abilityIncrease;
 }
