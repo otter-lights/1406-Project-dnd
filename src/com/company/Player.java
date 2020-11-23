0521234bc3278f1878b938f8c71b4ee85c8e1628
awildecat <awildecat@gmail.com>
2020-11-22 19:45:19 -0500

    Update Player.java

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -13,8 +13,27 @@ public class Player {
     int wisdom;
     int charisma;
     boolean Alive;
+    int[] money = new int[3]; // depends on class
+
+    public Player(){
+
+    }
 
     public void getRacialBonus(){
 
     }
+
+    public void getStartingEquipment(){
+        //depends on class
+    }
+
+    public void getSpells(){
+        //depends on class
+    }
+
+    private void doRolls(){
+
+    }
+
+
 }
