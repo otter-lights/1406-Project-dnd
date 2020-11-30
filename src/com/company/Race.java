ec2043187b12f6408fef203dfba46bfcaa6714dc
otter-lights <rachelroutly@cmail.carleton.ca>
2020-11-29 19:47:33 -0500

    Dealing with Level + Proficency Bonus based on XP

--- src/com/company/Race.java
+++ src/com/company/Race.java
@@ -1,6 +1,5 @@
 package com.company;
 import java.util.Random;
-import java.util.ArrayList;
 
 
 public class Race {
@@ -13,115 +12,116 @@ public class Race {
     int[] abilityIncrease = new int[6];
 
     public Race(String chosenRace){
-        if(chosenRace.equals("Gnome")){
-            //Rock Gnome Stats
-            size = "Small";
-            baseSpeed = 25;
-            darkvision = true;
+        switch (chosenRace) {
+            case "Gnome":
+                //Rock Gnome Stats
+                size = "Small";
+                baseSpeed = 25;
+                darkvision = true;
 
-            //intelligence increase
-            abilityIncrease[3] = 2;
-            //con increase
-            abilityIncrease[2] = 1;
-        }
-        else if(chosenRace.equals("Halfling")){
-            size = "Small";
-            baseSpeed = 25;
-            darkvision = false;
-            // sets the dex increase to +2
-            abilityIncrease[1] = 2;
-            // sets the con increase to +1
-            abilityIncrease[2] = 1;
-        }
-        else if(chosenRace.equals("Dwarf")){
-            //Hill Dwarf
-            size = "Medium";
-            baseSpeed = 25;
-            darkvision = true;
+                //intelligence increase
+                abilityIncrease[3] = 2;
+                //con increase
+                abilityIncrease[2] = 1;
+                break;
+            case "Halfling":
+                size = "Small";
+                baseSpeed = 25;
+                darkvision = false;
+                // sets the dex increase to +2
+                abilityIncrease[1] = 2;
+                // sets the con increase to +1
+                abilityIncrease[2] = 1;
+                break;
+            case "Dwarf":
+                //Hill Dwarf
+                size = "Medium";
+                baseSpeed = 25;
+                darkvision = true;
 
-            //con increase
-            abilityIncrease[2] = 2;
-            //wis increase
-            abilityIncrease[4] = 1;
-        }
-        else if(chosenRace.equals("Dragonborn")){
-            size = "Medium";
-            baseSpeed = 30;
-            darkvision = false;
+                //con increase
+                abilityIncrease[2] = 2;
+                //wis increase
+                abilityIncrease[4] = 1;
+                break;
+            case "Dragonborn":
+                size = "Medium";
+                baseSpeed = 30;
+                darkvision = false;
 
-            //strength increase
-            abilityIncrease[0] = 2;
-            //charisma increase
-            abilityIncrease[5] = 1;
-        }
-        else if(chosenRace.equals("Elf")){
-            //wood elf
-            size = "Medium";
-            baseSpeed = 30;
-            darkvision = true;
+                //strength increase
+                abilityIncrease[0] = 2;
+                //charisma increase
+                abilityIncrease[5] = 1;
+                break;
+            case "Elf":
+                //wood elf
+                size = "Medium";
+                baseSpeed = 30;
+                darkvision = true;
 
-            //dex increase
-            abilityIncrease[1] = 2;
-            //wis increase
-            abilityIncrease[4] = 1;
-        }
-        else if(chosenRace.equals("HalfElf")){
-            size = "Medium";
-            baseSpeed = 30;
-            darkvision = true;
+                //dex increase
+                abilityIncrease[1] = 2;
+                //wis increase
+                abilityIncrease[4] = 1;
+                break;
+            case "HalfElf":
+                size = "Medium";
+                baseSpeed = 30;
+                darkvision = true;
 
-            //charisma increase
-            abilityIncrease[5] = 2;
+                //charisma increase
+                abilityIncrease[5] = 2;
 
-            Random rand = new Random();
-            ArrayList<Integer> options = new ArrayList<>();
-            int firstIncrease = rand.nextInt(5);
-            int secondIncrease = rand.nextInt(5);
+                Random rand = new Random();
+                int firstIncrease = rand.nextInt(5);
+                int secondIncrease = rand.nextInt(5);
 
-            while(secondIncrease == firstIncrease){
-                secondIncrease = rand.nextInt(5);
-                if(secondIncrease != firstIncrease){
-                    break;
+                while (secondIncrease == firstIncrease) {
+                    secondIncrease = rand.nextInt(5);
+                    if (secondIncrease != firstIncrease) {
+                        break;
+                    }
                 }
-            }
-            abilityIncrease[firstIncrease] = 1;
-            abilityIncrease[secondIncrease] = 1;
-        }
-        else if(chosenRace.equals("HalfOrc")){
-            size = "Medium";
-            baseSpeed = 30;
-            darkvision = true;
+                abilityIncrease[firstIncrease] = 1;
+                abilityIncrease[secondIncrease] = 1;
+                break;
+            case "HalfOrc":
+                size = "Medium";
+                baseSpeed = 30;
+                darkvision = true;
 
-            //strength increase
-            abilityIncrease[0] = 2;
-            //con increase
-            abilityIncrease[2] = 1;
-        }
-        else if(chosenRace.equals("Human")){
-            size = "Medium";
-            baseSpeed = 30;
-            darkvision = false;
+                //strength increase
+                abilityIncrease[0] = 2;
+                //con increase
+                abilityIncrease[2] = 1;
+                break;
+            case "Human":
+                size = "Medium";
+                baseSpeed = 30;
+                darkvision = false;
 
-            //+1 to all scores
-            abilityIncrease[0] = 1;
-            abilityIncrease[1] = 1;
-            abilityIncrease[2] = 1;
-            abilityIncrease[3] = 1;
-            abilityIncrease[4] = 1;
-            abilityIncrease[5] = 1;
-        }
-        else if(chosenRace.equals("Tiefling")){
-            size = "Medium";
-            baseSpeed = 30;
-            darkvision = true;
+                //+1 to all scores
+                abilityIncrease[0] = 1;
+                abilityIncrease[1] = 1;
+                abilityIncrease[2] = 1;
+                abilityIncrease[3] = 1;
+                abilityIncrease[4] = 1;
+                abilityIncrease[5] = 1;
+                break;
+            case "Tiefling":
+                size = "Medium";
+                baseSpeed = 30;
+                darkvision = true;
 
-            //intelligence increase
-            abilityIncrease[3] = 1;
-            //charisma increase
-            abilityIncrease[5] = 2;
-        }
-        else{
-            System.out.println("Invaild Race");
+                //intelligence increase
+                abilityIncrease[3] = 1;
+                //charisma increase
+                abilityIncrease[5] = 2;
+                break;
+            default:
+                System.out.println("Invaild Race");
+                break;
         }
     }
     public String getSize(){
