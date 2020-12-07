6a180005e3fd36a87c1fe6a05aa81705fca13193
awildecat <awildecat@gmail.com>
2020-12-06 21:51:19 -0500

    doing table stuff for non magic users

--- src/com/company/Monk.java
+++ src/com/company/Monk.java
@@ -2,7 +2,12 @@ package com.company;
 
 public class Monk extends NonMagicUser{
     //strength = 0, dexterity = 1, constitution = 2, intelligence = 3, wisdom = 4, charisma = 5
+    int[] martialArts = new int[] {4, 4, 4, 4, 6, 6, 6, 6, 6, 6, 8, 8, 8, 8, 8, 8, 10, 10, 10, 10};
+    //unarmored defense:
+    //AC = 10 + dex + wisdom
 
+    //if no weapon & no armor:
+    //attack with martialArts ^
     public Monk(String chosenRace){
         super(chosenRace, 35, 0, 8, new boolean[] {true, true, false, false, false, false});
     }
