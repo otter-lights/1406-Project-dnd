03cc652af7d2bd8532a692a6adfb08b84cb264ca
awildecat <awildecat@gmail.com>
2020-12-06 20:18:56 -0500

    setting stuff up!

--- src/com/company/Weapon.java
+++ src/com/company/Weapon.java
@@ -6,6 +6,7 @@ public class Weapon extends Item{
     private String type;
     private boolean twoHanded;
     private boolean melee; //true -> melee, false -> ranged
+    private int range = -1;
     public Weapon(String name, int cost, double weight, int damage, String type, boolean twoHanded, boolean melee){
         super(cost, weight);
         this.name = name;
@@ -14,4 +15,12 @@ public class Weapon extends Item{
         this.twoHanded = twoHanded;
         this.melee = melee;
     }
+    //for ranged weapons
+    public Weapon(String name, int cost, double weight, int damage, String type, boolean twoHanded, boolean melee, int range){
+        super(cost, weight);
+        this.name = name;
+        this.damage = damage;
+        this.type = type;
+        this.twoHanded = twoHanded;
+        this.melee = melee;
 }
