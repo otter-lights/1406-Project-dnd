7a4c341ce14e22c9d17a5d9d4da12679e588648d
awildecat <awildecat@gmail.com>
2020-12-09 22:18:03 -0500

    melee weapon tostring yay

--- src/com/company/Melee.java
+++ src/com/company/Melee.java
@@ -8,4 +8,14 @@ public class Melee extends Weapon{
     public Melee(String name, int cost, double weight, int damage){
         super(name, cost, weight, damage);
     }
+
+    public String toString(){
+        if (this.getCost() < 10){
+            return "A relatively cheap (compared to other stuff) " + getDamageType()  + " melee weapon. It weighs " + getWeight() + " lbs, costs " + getCost() + " gold piece(s), and does 1d" + getDamage() + " damage. " + getTwoHanded();
+        } else if (this.getCost() < 25){
+            return "An expensive " + getDamageType()  + " melee weapon. It weighs " + getWeight() + " lbs, costs " + getCost() + " gold piece(s), and does 1d" + getDamage() + " damage. " + getTwoHanded();
+        } else {
+            return "A VERY EXPENSIVE  " + getDamageType()  + " melee weapon. It weighs " + getWeight() + " lbs, costs " + getCost() + " gold piece(s), and does 1d" + getDamage() + " damage. " + getTwoHanded();
+        }
+    }
 }
