23c1b13c8ad1ef4de3f4f218b476142a48488f6d
awildecat <awildecat@gmail.com>
2020-12-11 20:40:55 -0500

    added descriptions

--- src/com/company/CharacterCreatorView.java
+++ src/com/company/CharacterCreatorView.java
@@ -14,24 +14,24 @@ public class CharacterCreatorView extends Pane {
     private String[] races = new String[] {"Dragonborn", "Dwarf", "Gnome", "Elf", "Half Elf", "Half Orc", "Halfling", "Human", "Tiefling"};
     private String[] classes = new String[] {"Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Rogue", "Sorcerer", "Wizard"};
     HashMap<String, String> descriptons = new HashMap<String,String>() {{
-        put("Dragonborn", "");
-        put("Dwarf", "");
-        put("Gnome", "");
-        put("Elf", "");
-        put("Half Elf", "");
-        put("Half Orc", "");
-        put("Halfling", "");
-        put("Human", "");
-        put("Tielfing", "");
-        put("Barbarian", "");
-        put("Bard", "");
-        put("Cleric", "");
-        put("Druid", "");
-        put("Fighter", "");
-        put("Monk", "");
-        put("Rogue", "Rogues");
-        put("Sorcerer", "");
-        put("Wizard", "");
+        put("Dragonborn", "Dragonborn look very much like dragons standing erect in humanoid form, though they lack wings or a tail.");
+        put("Dwarf", "Bold and hardy, dwarves are known as skilled warriors, minors, and workers of stone and metal.");
+        put("Gnome", "A gnome's energy and enthusiasm for living shines through every inch of their tiny body.");
+        put("Elf", "Elves are a magical people of otherworldly grace, living in the world but not entirely part of it.");
+        put("Half-Elf", "Half-elves combine what some say are the best qualities of their elf and human parents.");
+        put("Half-Orc", "Half-orcs' grayist pigmentation, sloping foreheads, jutting jaws, prominent teeth, and towering builds make their orcish heritage plain for all to see.");
+        put("Halfling", "The diminutive halflings survive in a world full of larger creatures by avoiding notice or, barring that, avoiding offense.");
+        put("Human", "Humans are the most adaptable and ambitious people among the common races. Whatever drives them, humans are the innovators, the achievers, and the pioneers of the worlds.");
+        put("Tielfing", "To be greeted with stares and whispers, to suffer violence and insult on the street, to see mistrust and fear in every eye: this is the lot of the tiefling.");
+        put("Barbarian", "A fierce warrior of primitive background who can enter a battle rage.");
+        put("Bard", "An inspiring magician whose power echoes the music of creation.");
+        put("Cleric", "A priestly champion who wields divine magic in service of a higher power.");
+        put("Druid", "A priest of the Old Faith, wielding the powers of nature and adopting animal forms.");
+        put("Fighter", "A master of martial combat, skilled with a variety of weapons and armor.");
+        put("Monk", "A master of martial arts, harnessing the power of the body in pursuit of physical and spiritual perfection.");
+        put("Rogue", "A scoundrel who uses stealth and trickery to overcome obstacles and enemies.");
+        put("Sorcerer", "A spell-caster who draws on inherent magic from a gift or bloodline.");
+        put("Wizard", "A scholarly magic-user capable of manipulating the structures of reality.");
     }};
 
     public CharacterCreatorView() {
