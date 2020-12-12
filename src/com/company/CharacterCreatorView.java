7cac9862d03c8c0c5bfa1dcdaa277740006c0fe9
awildecat <awildecat@gmail.com>
2020-12-11 19:51:19 -0500

    added combo boxes!

--- src/com/company/CharacterCreatorView.java
+++ src/com/company/CharacterCreatorView.java
@@ -1,12 +1,17 @@
 package com.company;
+import javafx.collections.FXCollections;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ListView;
 import javafx.scene.text.TextAlignment;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.TilePane;
 
 public class CharacterCreatorView extends Pane {
+    private String[] races = new String[] {"Dragonborn", "Dwarf", "Gnome", "Elf", "Half Elf", "Half Orc", "Halfling", "Human", "Tiefling"};
+    private String[] classes = new String[] {"Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Rouge", "Sorcerer", "Wizard"};
 
     public CharacterCreatorView() {
         Pane storeView = new Pane();
@@ -23,8 +28,17 @@ public class CharacterCreatorView extends Pane {
             make race & class drop down menus!!
          */
 
+        ComboBox raceSelection = new ComboBox(FXCollections.observableArrayList(races));
+        raceSelection.setPrefSize(150, 45);
+        raceSelection.relocate(100,100);
+
+        ComboBox classSelection = new ComboBox(FXCollections.observableArrayList(classes));
+        classSelection.setPrefSize(150, 45);
+        classSelection.relocate(100,200);
+
+
         //add ze children
-        storeView.getChildren().addAll();
+        storeView.getChildren().addAll(raceSelection, classSelection);
 
         getChildren().addAll(storeView);
     }
